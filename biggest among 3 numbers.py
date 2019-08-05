ff1cfb0270ab05544a0cc35edd9b2eb9cfdf3a67
ramkumar1819 <53602808+ramkumar1819@users.noreply.github.com>
2019-08-05 13:45:29 +0530

    Create biggest among 3 numbers.py

---
+++ biggest among 3 numbers.py
@@ -0,0 +1,9 @@
+a=int(input("enter the 1st number"))
+b=int(input("enter the 2nd number"))
+c=int(input("enter the 3rd number"))
+if a>b and a>c:
+    print("%d is big"%a)
+elif b>c:
+    print("%d is big"%b)
+else:
+    print("%d is big"%c)
