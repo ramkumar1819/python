87476d0f35e47eba26dbf795bc9a4aaac0dfcb31
ramkumar1819 <53602808+ramkumar1819@users.noreply.github.com>
2019-08-06 12:08:10 +0530

    Create list using membership.py

---
+++ list using membership.py
@@ -0,0 +1,8 @@
+#Python program to check whether a specified value is contained in a group of values
+
+x=[1,2,3,4,5,6,7]
+a=int(input("enter the value"))
+if a in x:
+    print("true")
+else:
+    print("false")
