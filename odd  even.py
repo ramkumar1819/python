2c836195418d0bac90f69f18ff36408f647a88c4
ramkumar1819 <53602808+ramkumar1819@users.noreply.github.com>
2019-08-06 11:39:50 +0530

    Rename odd or even.py to odd  even.py

---
+++ odd  even.py
@@ -0,0 +1,6 @@
+#Python program to find whether a given number (accept from the user) is even or odd, print out an appropriate message to the use
+a=int(input("enter the number"))
+if a%2==0:
+    print("%d is even"%a)
+else:
+    print("%d is odd"%a)
