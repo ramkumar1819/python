7367c971c963e177e4eb4591ea9a2089849f9d02
ramkumar1819 <53602808+ramkumar1819@users.noreply.github.com>
2019-08-06 11:18:47 +0530

    Update Is prg.py

--- Is prg.py
+++ Is prg.py
@@ -1,7 +1,7 @@
 # Python program to get a new string from a given string where "Is" has been added to the front. If the given string already begins with "Is" then return the string unchanged
 
 str1=str(input("enter the string"))
-a=str1[0:1]
+a=str1[0:2]
 str2="Is"
 if a==str2:
     print(str1)
