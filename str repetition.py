277f3e6fbe242347b11459e8ee91d4cf6eebb75e
ramkumar1819 <53602808+ramkumar1819@users.noreply.github.com>
2019-08-06 11:25:39 +0530

    Create str repetition.py

---
+++ str repetition.py
@@ -0,0 +1,5 @@
+# Python program to get a string which is n (non-negative integer) copies of a given string.
+
+str1=str(input("enter the string"))
+n=int(input("enter the number of times to print"))
+print(str1*n)
