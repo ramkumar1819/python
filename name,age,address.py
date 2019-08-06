2f5c7788e26c454be9a6b7199cb94bcb2a94155a
ramkumar1819 <53602808+ramkumar1819@users.noreply.github.com>
2019-08-06 14:16:32 +0530

    Create name,age,address.py

---
+++ name,age,address.py
@@ -0,0 +1,5 @@
+# Python program to display your details like name, age, address in three different lines.
+
+detaildict={"name":"ram","age":20,"address":"chennai"}
+for i,j in detaildict.items():
+    print(i,j)
