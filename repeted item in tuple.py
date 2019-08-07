f8a825997380e488dd479bdbfc1de18c8ef6ede4
ramkumar1819 <53602808+ramkumar1819@users.noreply.github.com>
2019-08-07 14:56:54 +0530

    Create repeted item in tuple.py

---
+++ repeted item in tuple.py
@@ -0,0 +1,6 @@
+#Python program to find the repeated items of a tuple.
+tup=(1,2,3,4,4)
+n=int(input("enter item need to check in tuple"))
+x=tup.count(n)
+print("%d appears %d times in tuple"%(n,x))
+
