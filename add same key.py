d42b8c3acf2815e3524b9f42f76a72e907dd996f
ramkumar1819 <53602808+ramkumar1819@users.noreply.github.com>
2019-08-09 12:37:41 +0530

    Create add same key.py

---
+++ add same key.py
@@ -0,0 +1,7 @@
+#Python program to combine two dictionary adding values for common keys
+d1={"x":1,"y":2}
+d2={"a":3,"x":5}
+for key in d2:
+    if key in d1:
+        d1[key]=d1[key]+d2[key]
+print(d2)
