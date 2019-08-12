6ecff94b15100e837fd25aec4575fb9d13675999
ramkumar1819 <53602808+ramkumar1819@users.noreply.github.com>
2019-08-12 08:59:18 +0530

    Create key,value,item.py

---
+++ key,value,item.py
@@ -0,0 +1,6 @@
+#Python program to get the key, value and item in a dictionary
+
+d={"name":"ram","age":18,"address":"chennai"}
+print("keys:",d.keys())
+print("values:",d.values())
+print("items:",d.items())
